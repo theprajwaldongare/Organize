0bece43c04299e9fbff95e64ba2376ff73446d25
Prajwal <prajwaldongare112@gmail.com>
2026-09-09 17:55:18 +0530

    feat: added files reading

--- agent.py
+++ agent.py
@@ -11,6 +11,40 @@ client = OpenAI(
     base_url="https://generativelanguage.googleapis.com/v1beta/"
 )
 
+folderUrl = "FolderToOrganize"
+
+def readFolderContent(folderUrl:str,relativePath: str = "",complete = []):
+
+    if not os.path.exists(folderUrl):
+        print("Folder path does not exists")
+        return
+    
+    items = os.listdir(folderUrl)
+
+    
+
+    if len(items)==0: # for empty folder to show 
+        print(f"{relativePath} [Empty Folder]")
+        complete.append(f"{relativePath} [Empty Folder]")
+        return
+    
+    for i in items:
+        
+        if i.startswith("."): # hidden files
+            continue 
+        currRelative = os.path.join(relativePath,i)
+        fullPath = os.path.join(folderUrl,i)
+
+        if os.path.isdir(fullPath):
+            readFolderContent(fullPath,currRelative,complete=complete)
+        else:
+            print(currRelative) # only when its file .. it create empty folder problem .. we never know empty folder exists .. 
+            complete.append(currRelative)
+
+    return complete
+
+allFolderFiles = readFolderContent(folderUrl)
+print(allFolderFiles)
 
 def apiCall(prompt:str):
     response = client.chat.completions.parse(
@@ -24,4 +58,4 @@ def apiCall(prompt:str):
     
     return result
 
-print(apiCall("hello!"))
+# print(apiCall("hello!"))
